8df66fdb793cdc95462ac571b180898014b8c033
vamshi-indla <vamshi.indla@gmail.com>
2017-01-07 18:16:59 -0500

    babynames

---
+++ babynames/babynames.py
@@ -0,0 +1,116 @@
+#!/usr/bin/python -tt
+# Copyright 2010 Google Inc.
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Google's Python Class
+# http://code.google.com/edu/languages/google-python-class/
+
+import sys
+import re
+
+"""Baby Names exercise
+
+Define the extract_names() function below and change main()
+to call it.
+
+For writing regex, it's nice to include a copy of the target
+text for inspiration.
+
+Here's what the html looks like in the baby.html files:
+...
+<h3 align="center">Popularity in 1990</h3>
+....
+<tr align="right"><td>1</td><td>Michael</td><td>Jessica</td>
+<tr align="right"><td>2</td><td>Christopher</td><td>Ashley</td>
+<tr align="right"><td>3</td><td>Matthew</td><td>Brittany</td>
+...
+
+Suggested milestones for incremental development:
+ -Extract the year and print it
+ -Extract the names and rank numbers and just print them
+ -Get the names data into a dict and print it
+ -Build the [year, 'name rank', ... ] list and print it
+ -Fix main() to use the extract_names list
+"""
+
+def extract_names(filename):
+  f = open(filename,'rU')
+  text = f.read()
+  match = re.search(r'Popularity in \d\d\d\d',text)
+  outrec = []
+  """
+    -Extract Year
+  """ 
+  if match: 
+    outrec.append(match.group().split()[-1])
+  else: 
+    print 'Year Not Found!!!'
+    sys.exit(0)
+
+  """
+   -Get the names and rank
+  """
+  babynames = re.findall(r'(<td>)(\d)(</td>)(<td>)(\w*)(</td>)(<td>)(\w*)(</td>)',text)
+  dict = {}
+  for babyname in babynames:
+    temp = babyname[4].lower()
+    if temp in dict:
+      if babyname[1] < dict[temp]:
+        dict[temp] = babyname[1]
+    else:
+      dict[temp]= babyname[1]
+      
+    temp = babyname[7].lower()
+    if temp in dict:
+      if babyname[1] < dict[temp]:
+        dict[temp] = babyname[1]
+    else:
+      dict[temp]= babyname[1]
+  
+  for namerank in sorted(dict.items()):
+    outrec.append(namerank[0] + ' ' + str(namerank[1]))
+     
+  f.close()
+  """
+  Given a file name for baby.html, returns a list starting with the year string
+  followed by the name-rank strings in alphabetical order.
+  ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
+  """
+  # +++your code here+++
+  return outrec
+
+
+def main():
+  # This command-line parsing code is provided.
+  # Make a list of command line arguments, omitting the [0] element
+  # which is the script itself.
+  args = sys.argv[1:]
+   
+  if not args:
+    print 'usage: [--summaryfile] file [file ...]'
+    sys.exit(1)
+
+  # Notice the summary flag and remove it from args if it is present.
+  summary = False
+  if args[0] == '--summaryfile':
+    summary = True
+    del args[0]
+    
+  # process all files provided in args  
+  for filename in args:
+    names = extract_names(filename)
+    text = '\n'.join(names)
+    if summary == True:
+      outf = open(filename + '.summary.txt', 'w')
+      outf.write(text + '\n')
+      outf.close()
+    else: 
+      print text
+     
+  # +++your code here+++
+  # For each filename, get the names, then either print the text output
+  # or write it to a summary file
+  
+if __name__ == '__main__':
+  main()
